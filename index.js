90a26bd8f99ab2707a466b87958ab09eaf460b84
Mhelvin Reyes <mhelvin.reyes@98labs.com>
2016-09-06 15:42:35 +0800

    output.json is the input, content is log at the console

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
-
+// cmArg = __dirname + /output.json
 var cmArg = process.argv.slice(2,3);
 var readStream = fs.createReadStream(cmArg[0]);
-var writeStream = fs.createWriteStream('output.json');
-
+var writeStream = fs.createWriteStream('writestream.json');
+var data = '';
+readStream.on('data', (chunk) => {
+	data+=chunk;
+});
+readStream.on('end', () => {
+	console.log(data);
+})
 readStream.pipe(writeStream);

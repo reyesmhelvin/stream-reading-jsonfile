b76a93826ca3ff054e30f3a3dc5f5f748fcf30b5
Mhelvin Reyes <mhelvin.reyes@98labs.com>
2016-09-07 10:00:57 +0800

    code revision

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+/* 
+file in current dir	 : node index.js output.json 
+file & specified dir : node index.js output.json c:\
+*/
 const fs = require('fs');
+const through2 = require('through2');
 
-var cmArg = process.argv.slice(2,3);
-var jsonfile = __dirname + '/' + cmArg[0];
-var readStream = fs.createReadStream(jsonfile);
-var writeStream = fs.createWriteStream('writestream.json');
-var data = '';
-readStream.on('data', (chunk) => {
-	data+=chunk;
-});
-readStream.on('end', () => {
-	console.log(data);
-})
-readStream.pipe(writeStream);
+var cmdArg = process.argv.slice(2,4);
+var dir = (cmdArg[1] === undefined) ? __dirname + '/' : cmdArg[1];
+var file = (cmdArg[1] === undefined) ? cmdArg[0] : dir + cmdArg[0];
+
+fs.createReadStream(file)
+.pipe(through2(function (chunk, enc, callback) {
+	this.push(chunk)
+	callback()
+})).pipe(process.stdout);

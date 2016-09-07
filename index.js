a9b602a6f43babd13d0455483d619f90bf109e8b
Mhelvin Reyes <mhelvin.reyes@98labs.com>
2016-09-07 15:34:54 +0800

    more refactoring

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-/* 
-file in current dir	 : node index.js output.json 
-*/
 const fs = require('fs');
 const through2 = require('through2');
 const path = require('path');
+const ERR = 'File does not exist!';
 
 var cmdArg = process.argv.slice(2,3);	
 var file = (arg) => {
 	if (path.isAbsolute(arg)) {	
-		fs.access(arg, (err) => {
-			if (err) throw 'File does not exist!';
-		});
-		
-	}else{
-		fs.access(path.resolve(process.cwd(), arg), (err) => {
-			if (err) throw 'File does not exist!';
-		});	
+		fs.access(arg, (err) => if (err) throw ERR);
+	} else {
+		fs.access(path.resolve(process.cwd(), arg), (err) => if (err) throw ERR);	
 	}
 	return arg;
 }
 
 fs.createReadStream(file(cmdArg[0]))
-.pipe(through2(function (chunk, enc, callback) {
+.pipe(through2((chunk, enc, callback) => {
 	this.push(chunk)
 	callback()
 })).pipe(process.stdout);

ac5478d6a967e4297ad80d907d57d2c94a85941e
Mhelvin Reyes <mhelvin.reyes@98labs.com>
2016-09-07 11:58:00 +0800

    fix double new line

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 /* 
 file in current dir	 : node index.js output.json 
-file & specified dir : node index.js output.json c:\
 */
 const fs = require('fs');
 const through2 = require('through2');
@@ -8,7 +7,6 @@ const path = require('path');
 
 var cmdArg = process.argv.slice(2,3);
 var file = path.resolve(process.cwd(), cmdArg[0]);
-
 
 fs.createReadStream(file)
 .pipe(through2(function (chunk, enc, callback) {

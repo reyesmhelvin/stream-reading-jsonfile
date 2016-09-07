eac46d1a11d6ccbc32d6af6cdc7d8e408c67ce85
Mhelvin Reyes <mhelvin.reyes@98labs.com>
2016-09-07 13:57:20 +0800

    code refactoring: fullfilling requirements, with error handling

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ const fs = require('fs');
 const through2 = require('through2');
 const path = require('path');
 
-var cmdArg = process.argv.slice(2,3);
-var file = path.resolve(process.cwd(), cmdArg[0]);
+var cmdArg = process.argv.slice(2,3);	
+var file = (arg) => {
+	if (path.isAbsolute(arg)) {	
+		fs.access(arg, (err) => {
+			if (err) throw 'File does not exist!';
+		});
+		
+	}else{
+		fs.access(path.resolve(process.cwd(), arg), (err) => {
+			if (err) throw 'File does not exist!';
+		});	
+	}
+	return arg;
+}
 
-fs.createReadStream(file)
+fs.createReadStream(file(cmdArg[0]))
 .pipe(through2(function (chunk, enc, callback) {
 	this.push(chunk)
 	callback()
